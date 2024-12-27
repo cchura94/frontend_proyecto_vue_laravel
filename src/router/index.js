9eccc0db191f91a2160f15b0c8a243b594842db6
cristian <cchura.cpc@gmail.com>
2024-12-26 22:35:52 -0400

    Login Interceptors y Guards

--- src/router/index.js
+++ src/router/index.js
@@ -6,18 +6,40 @@ import { createRouter, createWebHistory } from "vue-router";
 import Perfil from "../views/admin/perfil/Perfil.vue";
 
 const routes = [
-    { path: '/', component: Inicio },
-    { path: '/nosotros', component: Acerca },
+    { path: '/', component: Inicio, name: 'Inicio',  meta: {requireAuth: true} },
+    { path: '/nosotros', component: Acerca, name: 'Nosotros' },
 
-    { path: '/auth/login', component: Login },
-    { path: '/auth/register', component: Registro },
+    { path: '/auth/login', component: Login, name: 'Login', meta: {redirectIfAuth: true} },
+    { path: '/auth/register', component: Registro, name: 'Registro' },
 
-    { path: '/admin/perfil', component: Perfil },
+    { path: '/admin/perfil', component: Perfil, name: 'Perfil', meta: {requireAuth: true} },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes: routes
 })
+
+// Guards
+router.beforeEach((to, from, next) => {
+
+    // console.log(to)
+
+    const token = localStorage.getItem("access_token") || null;
+
+    if(to.meta.requireAuth){
+        if(!token){
+            return next({name: 'Login'})
+        }
+        return next()
+    }
+
+    if(to.meta.redirectIfAuth && token){
+        return next({name: 'Perfil'})
+    }
+
+    return next()
+});
+
 
 export default router;
--- src/views/admin/perfil/Perfil.vue
+++ src/views/admin/perfil/Perfil.vue
@@ -1,22 +1,28 @@
 <template>
     <h1>Mi Perfil</h1>
-    <div v-if="perfil">
+    <div v-if="perfil.name">
         <h3>NOMBRE: {{ perfil.name }}</h3>
         <h3>CORREO: {{ perfil.email }}</h3>
         <h3>CONTRASEÑA: {{ perfil.password }}</h3>
         <button @click="obtenerDatos()">Obtener Datos</button>
         {{ perfil }}
+        <br>
+        <button @click="funSalir()">SALIR (LOGOUT)</button>
     </div>
     <div v-else>
         <h1>Cargando...</h1>
     </div>
+
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue';
 import authService from '../../../services/auth.service';
+import { useRouter } from "vue-router"
 
 const perfil = ref({})
+
+const router = useRouter()
 
 onMounted(() => {
     getPerfil()
@@ -27,7 +33,6 @@ onMounted(() => {
 const getPerfil = async () => {
     const {data} = await authService.profile();
     perfil.value = data;
-
 }
 
 function obtenerDatos(){
@@ -35,5 +40,12 @@ function obtenerDatos(){
     console.log(perfil.value.email)
 } 
 
+const funSalir = async function (){
+    await authService.logout();
+    localStorage.removeItem("access_token");
+
+    router.push({name: "Login"});
+
+}
 
 </script>
